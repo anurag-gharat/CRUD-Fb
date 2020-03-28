1960bef45c42aedbab020eb1384897906643e58e
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-03-28 17:47:16 +0530

    ""final

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,18 +4,22 @@ import app from "../base";
 
 
 export default class Home extends Component {
+
     render() {
+
+        
         return (
             <div>
                 This is home
-                <button onClick={() => app.auth().signOut().then(  
-                    {if (currentUser) {
-                        console.log(currentUser) 
-                    }
-
-                    
-                    })}>Sign out</button>
+                <button onClick={() => this.logout()}>Sign out</button>
             </div>
         )
     }
+    logout=()=>{
+            
+        app.auth().signOut()
+            
+
+    }
+
 }
--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    
+  
 });
 
 export default app;
